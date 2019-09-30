030c7d2fbee237e7a20410f72931d8c7a5d07b10
David Espinola <daver787@gmail.com>
2019-09-30 12:06:07 -0500

    Cleaned up repo of ArcPy class.

--- EspinolaFinalScripts/ChangeFolderName.py
+++
@@ -1,7 +0,0 @@
-#Question 1.1-ChangeFolderName.py:Change the name of the folder LastnameScripts to your last name using a short Python script.
-import os
-
-os.chdir("C:/EspinolaFinal")#change the current directory so that path names are shorter
-print(os.getcwd())#print this out to verify it has been changed
-os.rename("LastnameScripts","EspinolaScripts")#change the name of the subfolder in EspinolaFinal to EspinolaScripts from LastnameScripts
-
